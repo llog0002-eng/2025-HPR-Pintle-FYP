7f946f373d4b22a2d818f2458097e562058ba383
Luke Logan <llog0002@student.monash.edu>
2025-09-19 00:27:48 +1000

    Added frame loading and plotting

--- Image Processing/img_processing.py
+++ Image Processing/img_processing.py
@@ -1,5 +1,19 @@
 import pyMRAW
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-images, info = pyMRAW.load_video("xxx.cihx")
+
+# This loads all frames into RAM
+images, info = pyMRAW.load_video(r"A:\OneDrive - Monash University\Uni\HPR\FYP\Testing data\Sample\test_footage.cih")
+print("Loaded video shape:", images.shape)
+print("Info:", info)
+print("Parsed header:")
+for k, v in info.items():
+    print(f"{k}: {v}")
+
+first_image = images[0]
+
+plt.figure()
+plt.imshow(first_image, cmap='gray')
+plt.show()
